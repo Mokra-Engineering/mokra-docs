55959cdc0f60c5c7a13cfc9e0a051717db822ebe
Peter Nsaka <nsakapeter@gmail.com>
2026-03-28 08:20:50 -0400

    Update UI

--- mokra.js
+++ mokra.js
@@ -3,7 +3,7 @@
   script.src = 'https://app.mokra.ai/embed.js';
   script.setAttribute('data-service', 'mokra');
   script.setAttribute('data-position', 'bottom-right');
-  script.setAttribute('data-button-text', 'Agent Mock Server');
+  script.setAttribute('data-button-text', 'Coding Agent Sandbox');
   script.setAttribute('data-button-color', '#0088ff');
   script.setAttribute('data-theme', 'light');
   document.head.appendChild(script);
