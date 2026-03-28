de04b6249db3429808b69c083f52701835facf4e
Peter Nsaka <nsakapeter@gmail.com>
2026-03-28 08:08:18 -0400

    Add Mokra to Docs

---
+++ mokra.js
@@ -0,0 +1,10 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://app.mokra.ai/embed.js';
+  script.setAttribute('data-service', 'mokra');
+  script.setAttribute('data-position', 'bottom-right');
+  script.setAttribute('data-button-text', 'Agent Mock Server');
+  script.setAttribute('data-button-color', '#0088ff');
+  script.setAttribute('data-theme', 'light');
+  document.head.appendChild(script);
+})();
